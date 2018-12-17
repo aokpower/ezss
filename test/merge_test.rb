7d0c469efcbbf09e70b5bd38a03716d1142ec55d
Cooper LeBrun <cooperlebrun@gmail.com>
2018-12-17 09:43:54 -0800

    extract raw rows in test #setup

--- test/merge_test.rb
+++ test/merge_test.rb
@@ -1,24 +1,22 @@
 require 'minitest/autorun'
 require_relative '../merge'
 
+
 class MergeTest < Minitest::Test
   def setup
-    @spreadsheet_a = Spreadsheet.new(
-      [%w[Foo bar baz],
-       [1, 2, 3],
-       [2, 4, 6],
-       [3, 8, 12],
-       [4, 16, 24]],
-      matcher_i: 0
-    )
-    @spreadsheet_b = Spreadsheet.new(
-      [%w[bish bash bosh Foo],
-       ['a', 'b', 'c', 1],
-       ['b', 'd', 'f', 2],
-       ['g', 'h', 'i', 4],
-       ['j', 'k', 'l', 4]],
-      matcher_i: 3
-    )
+    @a_raw = [%w[Foo bar baz],
+              [1, 2, 3],
+              [2, 4, 6],
+              [3, 8, 12],
+              [4, 16, 24]]
+
+    @b_raw = [%w[bish bash bosh Foo],
+              ['a', 'b', 'c', 1],
+              ['b', 'd', 'f', 2],
+              ['g', 'h', 'i', 4],
+              ['j', 'k', 'l', 4]]
+    @spreadsheet_a = Spreadsheet.new(@a_raw, matcher_i: 0)
+    @spreadsheet_b = Spreadsheet.new(@b_raw, matcher_i: 3)
   end
 
   def test_new_headers
