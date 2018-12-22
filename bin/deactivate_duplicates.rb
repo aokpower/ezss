67330fb048e5f5f298c49c4ec79464b4dda66d50
Cooper LeBrun <cooperlebrun@gmail.com>
2018-12-22 12:19:01 -0800

    add deactivate_duplicates script

---
+++ bin/deactivate_duplicates.rb
@@ -0,0 +1,17 @@
+require_relative '../lib/ezss'
+
+spreadsheet = Spreadsheet.from_file ARGV[0]
+
+duplicates = 0 # STATS
+spreadsheet.pick_matcher(0) # 0 = Master Sku index
+spreadsheet.map_duplicates! do |row|
+  row[3] = 'FALSE'
+  duplicates += 1
+end
+
+puts "There were #{duplicates} duplicates"
+
+print('Name of output file?: ')
+outfile = STDIN.gets.chomp
+
+spreadsheet.write(outfile)
