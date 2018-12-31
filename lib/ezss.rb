fe0eb51294925c15984c9e47ec8ff2d74e80e71e
Cooper LeBrun <cooperlebrun@gmail.com>
2018-12-31 09:06:32 -0800

    add minor doc comments to EZSS

--- lib/ezss.rb
+++ lib/ezss.rb
@@ -23,7 +23,11 @@ class Spreadsheet
     end
 
     private
-    
+
+    # Takes an array of hashes and merges values like this:
+    # { a: 1, b: [1] } -> { a: 2, b: [2] } -> { a: [[1], [2]], b: [[[1]], [[2]]] }
+    # NOTE: Might be worth to extract a to a HOF and have a better named
+    # function for #combine and #merge_search_results
     def merge_search_results(hs)
       hs.reduce do |ha, hb|
         ha.merge(hb) { |_, va, vb| [va] << vb }
@@ -41,6 +45,7 @@ class Spreadsheet
     @matcher_i = matcher_i
   end
 
+	# Useful for representing empty matches without screwing up csv formatting in #write
   def offset
     Array.new(headers.length, '')
   end
@@ -49,6 +54,9 @@ class Spreadsheet
     @matcher_i && headers[@matcher_i]
   end
 
+	# Search spreadsheet for rows with matcher == to arg
+	# :empty_offset : Return #offset value instead of empty array when no
+	# matching rows (DEFAULT: FALSE)
   def matcher_search(m, empty_offset: false)
     r = rows.select { |r| r[matcher_i] == m }
     if empty_offset
@@ -62,18 +70,22 @@ class Spreadsheet
     ms.map { |m| [m, matcher_search(m, empty_offset: empty_offset)] }.to_h
   end
 
+	# Return all matchers
   def match_data
     rows.map { |row| row[matcher_i] }
   end
 
+	# Set matcher_i either through arg or block which yields headers
   def pick_matcher(ind = nil)
     @matcher_i = ind || yield(headers)
   end
 
+	# yields row and spreadsheet and
   def map!
     @rows = @rows.map { |row| yield row, self }
   end
 
+	# Shortcut to pick matcher_i interactively from STDIN
   def pick_matcher_from_prompt
     pick_matcher do |headers|
       puts "Which header do you want to match#{" for #{name}" if name}?"
@@ -83,6 +95,8 @@ class Spreadsheet
     end
   end
 
+	# Writes headers and rows to file
+	# No way to write to object only yet as I don't think the csv lib can do that :(
   def write(filename)
     CSV.open(filename, 'w', force_quotes: true) do |csvf|
       csvf << headers
